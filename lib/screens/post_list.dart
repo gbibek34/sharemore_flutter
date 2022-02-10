77b0d1e674723450fdd50d1903f8b8f4d4fc1ed3
gbibek34 <gbibek34@gmail.com>
2022-02-11 03:47:02 +0545

    Created a post list to show all the blogs by current user

--- lib/screens/post_list.dart
+++ lib/screens/post_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:sharemore/models/postModel.dart';
 
 import 'package:sharemore/utilities/colors.dart';
 import 'package:sharemore/utilities/network_handler.dart';
+import 'package:sharemore/utilities/userfetch.dart';
 import 'package:sharemore/widgets/post_card.dart';
 import 'package:sharemore/widgets/sidebar.dart';
 import 'package:sharemore/widgets/topbar.dart';
@@ -30,19 +32,29 @@ class _PostListState extends State<PostList> {
   Future<List<postModel>> getPosts() async {
     List<postModel> temp_list = <postModel>[];
     var res = await networkHandler.get("/post/");
+    var user = await getUser();
+    Logger().i(user.username);
     var postData = res["msg"];
-    // posts = json.decoder["msg"];
     for (int i = 0; i < res["msg"].length; i++) {
-      temp_list.add(postModel(
-        title: postData[i]["title"],
-        description: postData[i]["description"],
-        image: postData[i]["image"],
-        username: postData[i]["username"],
-        category: postData[i]["category"],
-        createdAt: postData[i]["createdAt"],
-      ));
+      if (postData[i]["username"] == user.username) {
+        temp_list.add(postModel(
+          id: postData[i]["_id"],
+          title: postData[i]["title"],
+          description: postData[i]["description"],
+          image: postData[i]["image"],
+          username: postData[i]["username"],
+          category: postData[i]["category"],
+          createdAt: postData[i]["createdAt"],
+        ));
+      }
     }
     return Future.delayed(Duration.zero, () => temp_list);
+  }
+
+  dataRefresh() {
+    setState(() {
+      posts = getPosts();
+    });
   }
 
   @override
@@ -55,78 +67,74 @@ class _PostListState extends State<PostList> {
         child: Topbar(),
         preferredSize: Size(double.infinity, 60),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(children: [
-            //!Welcome Message
-            Container(
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.circular(15),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(children: [
+              //todo:Welcome Message
+              Container(
+                decoration: BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "My Posts",
+                      style: _textTheme.headline4,
+                    )
+                  ],
+                ),
               ),
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 20,
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    "My Posts",
-                    style: _textTheme.headline4,
-                  )
-                ],
-              ),
-            ),
-            //!Gap
-            SizedBox(height: 20),
-            //!Recent Blogs
-            Container(
-              child: Column(
-                children: [
-                  SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        FutureBuilder<List<postModel>>(
-                            future: posts,
-                            builder: (context, snapshot) {
-                              if (snapshot.hasData) {
-                                return Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.5,
-                                  child: ListView.builder(
-                                      itemCount: snapshot.data!.length,
-                                      itemBuilder: (context, index) {
-                                        return PostCard(
-                                          category:
-                                              snapshot.data![index].category!,
-                                          title: snapshot.data![index].title!,
-                                          description: snapshot
-                                              .data![index].description!,
-                                        );
-                                      }),
-                                );
-                              } else {
-                                return Container(
-                                  child: Center(
-                                      child: CircularProgressIndicator()),
-                                );
-                              }
-                            }),
-                        // Container(
-                        //   height: MediaQuery.of(context).size.height * 0.5,
-                        //   child: LayoutBuilder( ,builder: (context, index) {
-                        //     return PostCard();
-                        //   }),
-                        // )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ]),
+              Container(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          FutureBuilder<List<postModel>>(
+                              future: posts,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Container(
+                                    height:
+                                        MediaQuery.of(context).size.height * 1,
+                                    child: ListView.builder(
+                                        itemCount: snapshot.data!.length,
+                                        itemBuilder: (context, index) {
+                                          return PostCard(
+                                            post_id: snapshot.data![index].id!,
+                                            category:
+                                                snapshot.data![index].category!,
+                                            title: snapshot.data![index].title!,
+                                            description: snapshot
+                                                .data![index].description!,
+                                            parentRefresh: dataRefresh,
+                                          );
+                                        }),
+                                  );
+                                } else {
+                                  return Container(
+                                    child: Center(
+                                        child: CircularProgressIndicator()),
+                                  );
+                                }
+                              }),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]),
+          ),
         ),
       ),
     );
